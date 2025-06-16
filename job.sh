4ca65691611d2148b54f049a6b2bca26b4ec595b
Francisca Angulo Painel <fangulo@kultrun-frontend.admin.dominio.net>
2025-06-16 12:51:10 -0400

    job.sh

--- job.sh
+++ job.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-#SBATCH --job-name=test_name
+#SBATCH --job-name=sim_2048_2048
 #SBATCH --partition=mapu
 #SBATCH -N 1
-#SBATCH --ntasks-per-node=4
-#SBATCH --output=/home/fangulo/myforkfargo3d/fargo3d/test.out
-#SBATCH --error=/home/fangulo/myforkfargo3d/fargo3d/test.err
+#SBATCH --ntasks-per-node=32
+#SBATCH --output=/home/fangulo/myforkfargo3d/fargo3d/sim_2048_2048.out
+#SBATCH --error=/home/fangulo/myforkfargo3d/fargo3d/sim_2048_2048.err
 module load openmpi/4.1.5
 cd /home/fangulo/myforkfargo3d/fargo3d
-mpirun -np 4 ./fargo3d setups/dustydisk/dustydisk.par
+mpirun -np 32 ./fargo3d setups/dustydisk/dustydisk.par
