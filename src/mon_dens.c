0a8a767ea6c55112b0ab3a44ccfdc904e620f18e
Francisca Angulo Painel <fangulo@kultrun-frontend.admin.dominio.net>
2025-06-27 14:57:22 -0400

    cambio mon_dens

--- src/mon_dens.c
+++ src/mon_dens.c
@@ -7,15 +7,18 @@
 #include "fargo3d.h"
 //<\INCLUDES>
 
-void mon_dens_cpu () {
+void mon_dens_cpu() {
 
 //<USER_DEFINED>
   INPUT(Density);
   OUTPUT(Slope);
+  real xp = Xplanet; 
+  real yp = Yplanet;
+  real mass = MplanetVirtual;
 //<\USER_DEFINED>
 
 
-//<EXTERNAL>
+//<EXTERNAL>// real MplanetVirtual(1);
   real* dens = Density->field_cpu;
   real* interm = Slope->field_cpu;
   int pitch  = Pitch_cpu;
@@ -23,19 +26,28 @@ void mon_dens_cpu () {
   int size_x = Nx+2*NGHX;
   int size_y = Ny+2*NGHY;
   int size_z = Nz+2*NGHZ;
+  real rh = pow(mass/3./MSTAR, 1./3.)*sqrt(xp*xp+yp*yp);
 //<\EXTERNAL>
 
-//<INTERNAL>
+//<INTERNAL>/home/fangulo/myforkfargo3d/fargo3d/src/mon_dens.c
   int i;
   int j;
   int k;
   int ll;
+  real dist2;
+  real dx;
+  real dy;
+  real dz;
+  real planet_distance;
 //<\INTERNAL>
 
 //<CONSTANT>
 // real Sxi(Nx);
 // real Syk(Nz+2*NGHZ);
 // real InvVj(Ny+2*NGHY);
+// real Xplanet(1);
+// real Yplanet(1);
+// real MplanetVirtual(1);
 //<\CONSTANT>
 
 //<MAIN_LOOP>
@@ -52,8 +64,19 @@ void mon_dens_cpu () {
       for (i=0; i<size_x; i++ ) {
 #endif
 //<#>
+#ifdef CYLINDRICAL
+	dx = ymed(j)*cos(xmed(i))-xp;
+	dy = ymed(j)*sin(xmed(i))-yp;
+#endif
+	dist2 = dx*dx+dy*dy+dz*dz;
+  planet_distance=sqrt(dist2);
+
 	ll = l;
-	interm[ll] = dens[ll]*Vol(i,j,k);
+  if (planet_distance > rh)
+	  interm[ll] = 0.0;
+  else{
+	  interm[ll] = dens[ll]*Vol(i,j,k);
+  }
 //<\#>
 #ifdef X
       }
